b813fee9ca79bed3e0dfd618ed9885134198f173
Marco <marco_carrillo@yahoo.com>
2020-01-25 21:31:27 -0500

    Merged front end files and made changes to back end server

--- server.js
+++ server.js
@@ -7,56 +7,9 @@ function randomValueHex (len) {
         .slice(0,len);   // return required number of characters
 }
 
-//************************************************************/
-//  Function to be executed when path requested is /notes    */
-//************************************************************/
-function getHTMLNotes(req,res){
-    console.log(`Request to send file notes.html was fulfilled`);
-    res.sendFile(path.join(__dirname, "notes.html"));
-};
-
-//*******************************************************/
-//  Function to be executed when path requested is *    */
-//*******************************************************/
-function getHTMLIndex(req,res){
-    console.log(`Request to send file index.html was fulfilled`);
-    res.sendFile(path.join(__dirname, "index.html"));
-};
-
-//******************************************************************/
-//  Function to be executed when path requested is GET /api/notes  */
-//******************************************************************/
-function getAPINotes(req,res){
-    let notes=JSON.parse(fs.readFileSync('./notes.json'));                     //  Reading file
-    console.log(`Request to return all notes in JSON has been fulfilled`);     //  Message to console
-    return res.json(notes);                                                    //  returns all notes
-};
-
-//*******************************************************************/
-//  Function to be executed when path requested is POST /api/notes  */
-//*******************************************************************/
-function postAPINote(req,res){
-    let newNote=req.body;                                                       // Getting the note from the request
-    newNote.id=randomValueHex(12);;                                             // Adding random hexadecimal key
-    allNotes=JSON.parse(fs.readFileSync('./notes.json','utf8'));                // Reading the existing JSON file into an array
-    allNotes.push(newNote);                                                     // Adding the new note into the new array
-    fs.writeFileSync('./notes.json',JSON.stringify(allNotes));                  // Writting new file to hard disc
-    console.log(`Note id ${newNote.id} successfully added to the JSON file`);   // Success message    
-};
-
 //*********************************************************************/
 //  Function to be executed when path requested is DELETE /api/notes  */
 //*********************************************************************/
-function deleteNote(req,res){
-    let idToDelete = req.params.id;                                             // Reading ID of note to delete
-    console.log(idToDelete);
-    allNotes=JSON.parse(fs.readFileSync('./notes.json','utf8'));                // Reading the existing JSON file into an array
-    for(let i=0;i<allNotes.length;i++){
-        if(allNotes[i].id===idToDelete){allNotes.splice(i,1)};                  // Eliminating the array
-    };
-    fs.writeFileSync('./notes.json',JSON.stringify(allNotes));                  // Writing changes to disk
-    console.log(`Request to elimiante array ${idToDelete} has been fulfilled`);
-};
 
 //*******************************************************/
 //  Main server functionality.  Setting dependencies    */
@@ -75,18 +28,74 @@ let app=express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-//*****************************************/
-//  Setting all routes for get and posts
-//*****************************************/
-app.get('/notes',getHTMLNotes);
-app.get('/api/notes',getAPINotes);
-app.get('/*',getHTMLIndex);
-app.post('/api/notes',postAPINote);
-app.delete('/api/notes/:id',deleteNote);
+//******************************************************************************/
+//  Setting all static routes to server files required to run the application
+//******************************************************************************/
+
+// ROOT: "/" -> Serve index.html
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// path /notes -> Called by index.html -> Serve notes.html
+app.get('/notes',function(req,res){
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+// CSS styling sheets -> Called by notes.html -> Serve styles.css
+app.get('/assets/css/styles.css',function(req,res){
+    res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"));
+});
+
+// index.js -> Called by notes.html -> Serve index.js
+app.get('/assets/js/index.js',function(req,res){
+    res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
+});
+
+
+//******************************************************************************/
+//  Setting all API GET routes to serve data to the application per requests
+//******************************************************************************/
+
+// /API/NOTES -> Servers data from DB/DB.JSON
+app.get('/api/notes',function(req,res){
+    let notes=JSON.parse(fs.readFileSync(path.join(__dirname,'/db/db.json'),'utf8'));     //  Reading file
+    console.log(`Request to return all notes in JSON has been fulfilled`);                //  Message to console
+    return res.json(notes);                                                               //  returns all notes
+});
+
+//******************************************************************************/
+//  Setting all API POST routes to serve data to the application per requests
+//******************************************************************************/
+
+// /API/NOTES -> Gets data and posts a new note
+app.post('/api/notes',function(req,res){
+    let newNote=req.body;                                                               // Getting the note from the request
+    newNote.id=randomValueHex(12);;                                                     // Adding random hexadecimal key
+    allNotes=JSON.parse(fs.readFileSync(path.join(__dirname,'/db/db.json'),'utf8'));    // Reading the existing JSON file into an array
+    allNotes.push(newNote);                                                             // Adding the new note into the new array
+    fs.writeFileSync(path.join(__dirname,'/db/db.json'),JSON.stringify(allNotes));      // Writting new file to hard disc
+    console.log(`Note id ${newNote.id} successfully added to the JSON file`);           // Success message    
+});
+
+
+//******************************************************************************/
+//  Setting all API DELETE routes to serve data to the application per requests
+//******************************************************************************/
+app.delete('/api/notes/:id',function(req,res){
+    console.log(req.params);
+    let idToDelete = req.params.id;                                                     // Reading ID of note to delete
+    allNotes=JSON.parse(fs.readFileSync(path.join(__dirname,'/db/db.json'),'utf8'));    // Reading the existing JSON file into an array
+    for(let i=0;i<allNotes.length;i++){
+        if(allNotes[i].id===idToDelete){allNotes.splice(i,1)};                          // Eliminating the array
+    };
+    fs.writeFileSync(path.join(__dirname,'/db/db.json'),JSON.stringify(allNotes));      // Writing changes to disk
+    console.log(`Request to eliminate array ${idToDelete} has been fulfilled`);
+});
 
 //**********************/
 //  Start listening
 //**********************/
 app.listen(serverPort,function(){
-    console.log('Started listening port 3000')
+    console.log(`Started listening port ${serverPort}`);
 })
